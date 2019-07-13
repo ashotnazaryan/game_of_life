66a122476601f691a50ea1d096eacf236c3a8778
Ashot <ashotnazaryan45@outlook.com>
2019-07-13 22:50:41 +0300

    dynamic matrix

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ export class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            size: 30,
             matrix: [],
+            initialMatrix: [],
             aliveCells: [],
-            started: false
+            processing: false
         };
     }
 
@@ -28,7 +30,7 @@ export class App extends Component {
                 matrix[i][j] = item;
             }
         }
-        this.setState({ matrix: matrix });
+        this.setState({ matrix: matrix, initialMatrix: JSON.parse(JSON.stringify(matrix)) });
         setTimeout(() => {
             this.findAllNeighbours();
         });
@@ -78,6 +80,18 @@ export class App extends Component {
         this.setState({ matrix: matrix });
     }
 
+    setSize = () => {
+        let { size } = this.state;
+        if (size >= 3 && size <= 50) {
+            this.setState({ size: size });
+            this.initMatrix(size);
+        }
+    }
+
+    updateInputValue = (event) => {
+        this.setState({ size: +event.target.value });
+    }
+
     componentDidMount() {
         this.initMatrix(30);
     }
@@ -87,75 +101,75 @@ export class App extends Component {
         cell.alive = !cell.alive;
         cell.alive ? aliveCells.push(cell) : aliveCells.splice(aliveCells.indexOf(cell), 1);
         this.setState({ matrix: matrix, aliveCells: aliveCells });
-        // console.log("matrix: ", matrix);
     }
 
     startGame = () => {
-        let { aliveCells, matrix } = this.state;
-        this.setState({ started: true });
-        for (let i = 0; i < aliveCells.length; i++) {
-            this.findNeighbours(aliveCells[i], matrix);
-            this._interval = setInterval(() => {
+        this._interval = setInterval(() => {
+            let { aliveCells, matrix } = this.state;
+            this.setState({ processing: true });
+            for (let i = 0; i < aliveCells.length; i++) {
+                this.findNeighbours(aliveCells[i], matrix);
                 this.findAliveNeighbours(aliveCells[i], matrix);
                 this.setState({ matrix: matrix });
-                console.log("interval: ", matrix);
-            }, 1000);
-        }
+            }
+        }, 1000);
     }
 
     stopGame = () => {
-        const { matrix } = this.state;
         clearInterval(this._interval);
-        this.setState({ started: false });
-        console.log("stop game: ", matrix);
+        this.setState({ processing: false });
     }
 
     resetBoard = () => {
-        const { matrix } = this.state;
+        const { initialMatrix } = this.state;
         clearInterval(this._interval);
-        this.initMatrix(30);
-        this.setState({ started: false });
-        console.log("reset board: ", matrix);
+        this.setState({ processing: false, matrix: initialMatrix });
     }
 
     render() {
-        const { matrix, started } = this.state;
+        const { matrix, processing, size } = this.state;
         return (
             <div className="App">
                 <div className="board-container">
-                    {
-                        matrix.map((rows, index) => {
-                            return (
-                                <div key={rows[index].coord} className="row">
-                                    {
-                                        rows.map((cell) => {
-                                            return (
-                                                <div key={cell.coord}
-                                                    className={`cell ${cell.alive ? 'alive' : ''}`}
-                                                    onClick={() => this.toggleCell(cell)}
-                                                >
+                    <div className="size-container">
+                        <input type="number" value={size} onChange={(event) => this.updateInputValue(event)} />
+                        <button onClick={() => this.setSize(size)}>Set size</button>
+                    </div>
+                    <div className="board">
+                        {
+                            matrix.map((rows, index) => {
+                                return (
+                                    <div key={rows[index].coord} className="row">
+                                        {
+                                            rows.map((cell) => {
+                                                return (
+                                                    <div key={cell.coord}
+                                                        className={`cell ${cell.alive ? 'alive' : ''}`}
+                                                        onClick={() => this.toggleCell(cell)}
+                                                    >
 
-                                                </div>
-                                            )
-                                        })
-                                    }
-                                </div>
-                            )
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                )
+                            }
+                            )}
+                    </div>
+                    <div className="actions">
+                        <button onClick={() => this.resetBoard()}>Reset board</button>
+                        {
+                            processing ?
+                                (
+                                    <button onClick={() => this.stopGame()}>Stop game</button>
+                                )
+                                :
+                                (
+                                    <button onClick={() => this.startGame()}>Start game</button>
+                                )
                         }
-                        )}
-                </div>
-                <div className="actions">
-                    <button onClick={() => this.resetBoard()}>Reset board</button>
-                    {
-                        started ?
-                            (
-                                <button onClick={() => this.stopGame()}>Stop game</button>
-                            )
-                            :
-                            (
-                                <button onClick={() => this.startGame()}>Start game</button>
-                            )
-                    }
+                    </div>
                 </div>
             </div>
         )
